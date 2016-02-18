c60e7044de95445593d47c941e13855ec547fa9a
RohitDeorey <rohit.deorey@gmail.com>
2016-02-19 01:49:22 +0530

    Rearrangement of folder structure for webApp. Also added routing for home page.

---
+++ TUGOnline/TUGWebApp/Home/Controllers/customerController.js
@@ -0,0 +1,3 @@
+﻿app.controller('customerHomeController', ['$scope', function ($scope) {
+    $scope.message = "This is home page";
+}]);
---
+++ TUGOnline/TUGWebApp/app.js
@@ -0,0 +1,9 @@
+﻿var app = angular.module("TUGApp", ['ngRoute']);
+
+app.config(function ($routeProvider) {
+    $routeProvider.when('/customerHome', {
+        templateUrl: 'Home/Views/customerHome.html',
+        controller: 'customerHomeController'
+    }).
+    otherwise({ redirectTo: '/customerHome' });
+});
