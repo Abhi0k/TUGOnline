0921f3f2fc050c68cdc635273ccd960ace773f6d
RohitDeorey <rohit.deorey@gmail.com>
2016-03-11 00:19:24 +0530

    Added Menu Items page.

--- TUGOnline/TUGWebApp/Home/Controllers/customerController.js
+++ TUGOnline/TUGWebApp/Home/Controllers/customerController.js
@@ -1,3 +1,27 @@
-﻿app.controller('customerHomeController', ['$scope', function ($scope) {
-    $scope.message = "This is home page";
+﻿app.controller('customerController', ['$scope', 'customerHomeMenuService', '$location', function ($scope, customerHomeMenuService, $location) {
+
+    //For hiding navigation items from first menu page
+    $scope.location = $location;
+    $scope.$watch('location.url()', getTitle);
+    function getTitle() {
+        $scope.pageTitle = $location.url().substring(1);
+    };
+    $scope.checkPageTitle = function() {
+        if ($scope.pageTitle == 'menuPage')
+            return true;
+        else
+            return false;
+    }
+
+    getMenuItems();
+    function getMenuItems() {
+        customerHomeMenuService.getMenu()
+            .success(function (data) {
+                $scope.menuItems = data;
+            })
+            .error(function (error) {
+                $scope.status = "error" + error.message;
+            });
+    };
+    
 }]);
---
+++ TUGOnline/TUGWebApp/Home/Services/customerHomeMenuService.js
@@ -0,0 +1,10 @@
+﻿app.factory('customerHomeMenuService', ['$http', function ($http) {
+    var menuObj = {};
+    var urlBase = "http://192.168.1.3/webapi/Api/Product/GetProducts";
+    
+    menuObj.getMenu = function () {
+        return $http.get(urlBase);
+    };
+
+    return menuObj;
+}]);
